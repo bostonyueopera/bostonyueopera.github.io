0fbf860563505ed75eba5ac1785957d5b3b5ceb1
emAmazed <emilieying@yahoo.com>
2019-02-08 13:18:17 -0500

    fix blog sidebar issue; temp add toggle sidebar css for future use

---
+++ js/toggle.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("blogSidenav").style.width = "285px";
+  document.getElementById("blogSidenav").style.padding = "0 10px 0 25px";
+  document.getElementById("blogSidenav").style.margin = "0 0 0 -48px";
+}
+
+function closeNav() {
+  document.getElementById("blogSidenav").style.width = "0";
+  document.getElementById("blogSidenav").style.padding = "0 0 0 0";
+  document.getElementById("blogSidenav").style.margin = "0 0 0 0";
+}
